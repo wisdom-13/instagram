0293504ca5dcbaae973ac9411413c5969115ce36
jhlee <wisdom_13_@naver.com>
2023-06-13 23:52:19 +0900

    현재 페이지 아이콘 표기

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,37 @@
+'use client'
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation'
+
+
 import { AiOutlineHome, AiFillHome } from 'react-icons/ai';
 import { BsPlusSquare, BsPlusSquareFill } from 'react-icons/bs';
 import { RiSearchLine, RiSearchFill } from 'react-icons/ri';
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <div className='w-1/5 max-w-sm h-screen	p-6 border border-r-gray-200'>
       <h1 className='mt-4 mb-9 text-2xl font-bold'>Instantgram</h1>
       <nav className=''>
         <Link className='flex items-center text-lg py-3' href='/'>
-          <AiOutlineHome className='mr-3' size='24' />홈
+          {pathname == "/"
+            ? <><AiFillHome className='mr-3' size='24' /><span className='font-bold'>홈</span></>
+            : <><AiOutlineHome className='mr-3' size='24' /><span>홈</span></>
+          }
         </Link>
         <Link className='flex items-center text-lg py-3' href='/search'>
-          <RiSearchLine className='mr-3' size='24' /> 검색
+          {pathname == "/search"
+            ? <><RiSearchFill className='mr-3' size='24' /><span className='font-bold'>검색</span></>
+            : <><RiSearchLine className='mr-3' size='24' /><span>검색</span></>
+          }
         </Link>
         <Link className='flex items-center text-lg py-3' href='/new'>
-          <BsPlusSquare className='mr-3' size='24' /> 만들기
+          {pathname == "/new"
+            ? <><BsPlusSquareFill className='mr-3' size='24' /><span className='font-bold'>만들기</span></>
+            : <><BsPlusSquare className='mr-3' size='24' /><span>만들기</span></>
+          }
         </Link>
       </nav>
       <Link className='flex items-center text-lg py-3' href='/'>
