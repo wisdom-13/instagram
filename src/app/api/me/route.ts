9d4ee79637c52bc71495e9d0ec82bed58fc122b4
jhlee <wisdom_13_@naver.com>
2023-06-29 06:11:56 +0900

    유저 정보 호출 route 생성

---
+++ src/app/api/me/route.ts
@@ -0,0 +1,14 @@
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+
+export async function GET(request: Request) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response('Authentication Error', { status: 401 });
+  }
+
+  return NextResponse.json('meeee');
+}
--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -3,7 +3,8 @@
 import useSWR from "swr";
 
 export default function FollowingBar() {
-  const { data, isLoading, error } = useSWR('')
+  const { data, isLoading, error } = useSWR('/api/me');
+  console.log(data)
   return (
     <div>
       FollowingBar
