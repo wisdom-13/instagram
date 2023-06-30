a8d6da4d03ee500b8e26403031e9cb0d2a467bca
jhlee <wisdom_13_@naver.com>
2023-06-30 18:23:54 +0900

    GROQ 쿼리 사용하기

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { authOptions } from "../auth/[...nextauth]/route";
+import { getUserByUsername } from "@/service/user";
 
 export async function GET(request: Request) {
   const session = await getServerSession(authOptions);
@@ -10,5 +11,7 @@ export async function GET(request: Request) {
     return new Response('Authentication Error', { status: 401 });
   }
 
-  return NextResponse.json('meeee');
+  return getUserByUsername(user.username)
+    .then((data) => NextResponse.json(data)
+    );
 }
--- src/service/user.ts
+++ src/service/user.ts
@@ -21,3 +21,15 @@ export async function addUser({ id, username, email, name, image }: OAuthUser) {
     bookmarks: [],
   })
 }
+
+export async function getUserByUsername(username: string) {
+  return client.fetch(
+    `*[_type == "user" && username == "${username}"][0]{
+      ...,
+      "id": _id,
+      following[] -> {username, image},
+      foolowers[] -> {username, image},
+      "bookmarks":bookmarks[]->id
+    }`
+  )
+}
