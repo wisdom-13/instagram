c1b5ea30f596d1f22dc731a44dcc9e3c67d75424
jhlee <wisdom_13_@naver.com>
2023-07-04 23:04:10 +0900

    포스트 카드 추가

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,6 +1,6 @@
 export default function PostList() {
   return (
-    <div>
+    <div className="w-[470px] bg-white p-3 border border-gray-200 rounded-lg text-center min-h-[112px] overflow-x-auto pr-0 mt-3">
       PostList
     </div>
   );
