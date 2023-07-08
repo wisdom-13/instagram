cee8ef41aa6b28f1aa5f3a67eedd6a20c43cb23b
jhlee <wisdom_13_@naver.com>
2023-07-08 23:48:46 +0900

    포스트 카드 컴포넌트 분리

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -2,11 +2,8 @@
 
 import { SimplePost } from "@/model/post";
 import useSWR from "swr";
-import Avatar from "./Avatar";
-import Image from "next/image";
-import HeartIcon from "./ui/icons/HeartIcon";
-import BookmarkIcon from "./ui/icons/BookmarkIcon";
-import ChatbubbleIcon from "./ui/icons/ChatbubbleIcon";
+import { GridLoader } from "react-spinners";
+import PostListCard from "./PostListCard";
 
 
 export default function PostList() {
@@ -16,34 +13,10 @@ export default function PostList() {
 
   return (
     <>
+      {isLoading && <div className="w-[470px] bg-white border border-gray-200 rounded-lg mt-3 py-10 text-center"><GridLoader color='red' /></div>}
       {
         posts && posts.map((post) => (
-
-          <div key={post.id} className="w-[470px] bg-white border border-gray-200 rounded-lg mt-3">
-            <div className="flex items-center p-3">
-              <Avatar image={post.userImage} size='medium' />
-              <h3 className="font-semibold pl-3">{post.username}</h3>
-            </div>
-            <div>
-              <Image src={post.image} width={470} height={470} alt={post.username} />
-            </div>
-            <div className="p-3">
-              <div className="flex justify-between">
-                <HeartIcon />
-                <BookmarkIcon />
-              </div>
-              <div className="text-sm py-3">
-                <b>좋아요 1개</b>
-              </div>
-              <div className="text-sm">
-                <p><b className="pr-1">{post.username}</b>{post.text}</p>
-              </div>
-            </div>
-            <div className="flex border-t border-gray-200 p-3 text-sm">
-              <input className="flex-grow outline-none" type="text" placeholder="댓글 달기..." />
-              <button className="text-blue-600 font-semibold w-[40px]">게시</button>
-            </div>
-          </div>
+          <PostListCard key={post.id} post={post} />
         ))
       }
     </>
---
+++ src/components/PostListCard.tsx
@@ -0,0 +1,41 @@
+import { SimplePost } from "@/model/post";
+import Avatar from "./Avatar";
+import HeartIcon from "./ui/icons/HeartIcon";
+import BookmarkIcon from "./ui/icons/BookmarkIcon";
+import Image from "next/image";
+
+type Props = {
+  post: SimplePost
+}
+
+export default function PostListCard({ post }: Props) {
+  const { userImage, username, image, text, likes } = post;
+  return (
+    <div className="w-[470px] bg-white border border-gray-200 rounded-lg mt-3">
+      <div className="flex items-center p-3">
+        <Avatar image={userImage} size='medium' />
+        <h3 className="font-semibold pl-3">{username}</h3>
+      </div>
+      <div>
+        <Image src={image} width={470} height={470} alt={username} />
+      </div>
+      <div className="p-3">
+        <div className="flex justify-between">
+          <HeartIcon />
+          <BookmarkIcon />
+        </div>
+        <div className="text-sm py-3">
+          <b>{`좋아요 ${likes?.length ?? 0}개`}</b>
+        </div>
+        <div className="text-sm">
+          <p><b className="pr-1">{username}</b>{text}</p>
+        </div>
+      </div>
+      <div className="flex border-t border-gray-200 p-3 text-sm">
+        <input className="flex-grow outline-none" type="text" placeholder="댓글 달기..." />
+        <button className="text-blue-600 font-semibold w-[40px]">게시</button>
+      </div>
+    </div>
+  );
+}
+
