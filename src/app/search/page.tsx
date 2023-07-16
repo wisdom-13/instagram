cf94ef277630b4936da2cd522cfbf50ecbded26c
jhlee <wisdom_13_@naver.com>
2023-07-16 23:52:49 +0900

    검색 페이지

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,35 @@
+'use client';
+
+import useSWR from "swr";
+import { DetailUser } from "@/model/user";
+import Image from "next/image";
+import Link from "next/link";
+import Avatar from "@/components/Avatar";
+
 export default function SearchPage() {
+  const { data, isLoading, error } = useSWR<DetailUser>('/api/me');
+  const users = data?.following;
+
   return (
-    <div>
-      SearchPage
+    <div className='w-full flex pt-6 justify-center'>
+      <form>
+        <input placeholder="Search for a email or name" />
+      </form>
+      <div>
+        <div>
+          {users && users.map(({ image, username }) =>
+            <Link href={`/user/${username}`} key={username}>
+              <div className='flex'>
+                <Avatar image={image} size='medium' />
+                <div>
+                  <b>{username}</b>
+                  {username}
+                </div>
+              </div>
+
+            </Link>)}
+        </div>
+      </div>
     </div>
   );
 }
