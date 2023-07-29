1f9ab17b9930ccb774b58b80f71b20e9bad83261
jhlee <wisdom_13_@naver.com>
2023-07-29 18:00:34 +0900

    좋아요 라우트 핸들러

--- src/service/post.ts
+++ src/service/post.ts
@@ -71,3 +71,21 @@ function mapPosts(posts: SimplePost[]) {
     ...post, image: urlFor(post.image)
   }));
 }
+
+export async function likePost(postId: string, userId: string) {
+  return client.patch(postId)
+    .setIfMissing({ likes: [] })
+    .append('likes', [
+      {
+        _ref: userId,
+        _type: 'reference'
+      }
+    ])
+    .commit({ autoGenerateArrayKeys: true })
+}
+
+export async function dislikePost(postId: string, userId: string) {
+  return client.patch(postId)
+    .unset([`likes[_ref=="${userId}"]`])
+    .commit()
+}
