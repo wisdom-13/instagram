a78b68da60e0cad6e8a255ad392fa9f6a61f67e0
jhlee <wisdom_13_@naver.com>
2023-07-29 18:34:36 +0900

    좋아요 버튼 커스텀 훅으로 분리

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -8,6 +8,7 @@ import BookmarkFillIcon from "./ui/icons/BookmarkFillIcon";
 import { SimplePost } from "@/model/post";
 import { useSession } from "next-auth/react";
 import { useSWRConfig } from "swr";
+import usePosts from "@/hooks/usePosts";
 
 type Props = {
   post: SimplePost
@@ -21,13 +22,12 @@ export default function ActionBar({ post }: Props) {
   const liked = user ? likes.includes(user.username) : false;
   const [bookmarked, setBookmarked] = useState(false);
 
-  const { mutate } = useSWRConfig();
+  const { setLike } = usePosts();
 
   const handleLike = (like: boolean) => {
-    fetch('api/likes', {
-      method: 'PUT',
-      body: JSON.stringify({ id, like })
-    }).then(() => mutate('/api/posts'))
+    if (user) {
+      setLike(post, user.username, like)
+    }
   }
 
   return (
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { SimplePost } from "@/model/post";
-import useSWR from "swr";
 import PostListCard from "./PostListCard";
 import GridSpinner from "./ui/GridSpinner";
+import usePosts from "@/hooks/usePosts";
 
 
 export default function PostList() {
-  const { data: posts, isLoading, error } = useSWR<SimplePost[]>('/api/posts');
+  const { posts, isLoading, error } = usePosts();
   return (
     <>
       {isLoading &&
