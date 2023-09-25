0c5d59d68204b181e337e7f605a4f89c085d9fd3
jhlee <wisdom_13_@naver.com>
2023-09-26 03:29:02 +0900

    sanity config 수정

--- sanity-studio/sanity.cli.ts
+++ sanity-studio/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'm5fbywpb',
+    projectId: 'qelofgo5',
     dataset: 'production'
   }
 })
--- sanity-studio/sanity.config.ts
+++ sanity-studio/sanity.config.ts
@@ -1,16 +1,16 @@
-import {defineConfig, isDev} from 'sanity'
-import {visionTool} from '@sanity/vision'
-import {deskTool} from 'sanity/desk'
-import {schemaTypes} from './schemas'
-import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+import { defineConfig, isDev } from 'sanity'
+import { visionTool } from '@sanity/vision'
+import { deskTool } from 'sanity/desk'
+import { schemaTypes } from './schemas'
+import { getStartedPlugin } from './plugins/sanity-plugin-tutorial'
 
 const devOnlyPlugins = [getStartedPlugin()]
 
 export default defineConfig({
   name: 'default',
-  title: 'salmon-dotterel',
+  title: 'white-lark',
 
-  projectId: 'm5fbywpb',
+  projectId: 'qelofgo5',
   dataset: 'production',
 
   plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
